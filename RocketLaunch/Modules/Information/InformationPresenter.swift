2af5526f6de779ea8040241bc2d2454600cdec17
Brais P. Castro <braispcastro@gmail.com>
2022-01-16 20:22:11 +0100

    - Removed dismiss button on settings - Added info and wiki buttons for provider card

--- RocketLaunch/Modules/Information/InformationPresenter.swift
+++ RocketLaunch/Modules/Information/InformationPresenter.swift
@@ -32,50 +32,50 @@ final class InformationPresenter<T: InformationViewControllerProtocol, U: Inform
         
         sections.append(Information.InformationType.launch)
         let launch = Information.Launch(imageUrl: fullLaunch.image,
-                                                    rocket: fullLaunch.rocket?.configuration?.name ?? "-",
-                                                    mission: fullLaunch.mission?.name ?? "-",
-                                                    provider: fullLaunch.launchServiceProvider?.name ?? "-",
-                                                    pad: fullLaunch.pad?.name ?? "-",
-                                                    windowStart: fullLaunch.windowStart ?? "-",
-                                                    status: fullLaunch.status?.name ?? "-",
-                                                    statusType: fullLaunch.status?.type ?? .unknown)
+                                        rocket: fullLaunch.rocket?.configuration?.name ?? "-",
+                                        mission: fullLaunch.mission?.name ?? "-",
+                                        provider: fullLaunch.launchServiceProvider?.name ?? "-",
+                                        pad: fullLaunch.pad?.name ?? "-",
+                                        windowStart: fullLaunch.windowStart ?? "-",
+                                        status: fullLaunch.status?.name ?? "-",
+                                        statusType: fullLaunch.status?.type ?? .unknown)
         
         var provider: Information.Provider?
         if let rawProvider = fullLaunch.launchServiceProvider {
             sections.append(Information.InformationType.provider)
             provider = Information.Provider(logoUrl: rawProvider.logoUrl,
-                                                        name: rawProvider.name ?? "-",
-                                                        description: rawProvider.description ?? "-",
-                                                        countryCode: rawProvider.countryCode ?? "-",
-                                                        foundingYear: rawProvider.foundingYear ?? "-",
-                                                        infoUrl: rawProvider.infoUrl,
-                                                        wikiUrl: rawProvider.wikiUrl)
+                                            name: rawProvider.name ?? "-",
+                                            description: rawProvider.description ?? "-",
+                                            countryCode: rawProvider.countryCode ?? "-",
+                                            foundingYear: rawProvider.foundingYear ?? "-",
+                                            infoUrl: rawProvider.infoUrl,
+                                            wikiUrl: rawProvider.wikiUrl)
         }
         
         var mission: Information.Mission?
         if let rawMission = fullLaunch.mission {
             sections.append(Information.InformationType.mission)
             mission = Information.Mission(logoUrl: fullLaunch.missionPatches?.first?.imageUrl,
-                                                      name: rawMission.name ?? "-",
-                                                      type: rawMission.type ?? "-",
-                                                      description: rawMission.description ?? "-")
+                                          name: rawMission.name ?? "-",
+                                          type: rawMission.type ?? "-",
+                                          description: rawMission.description ?? "-")
         }
         
         var pad: Information.Pad?
         if let rawPad = fullLaunch.pad {
             sections.append(Information.InformationType.pad)
             pad = Information.Pad(name: rawPad.name ?? "-",
-                                              location: rawPad.location?.name ?? "-",
-                                              latitude: rawPad.latitude,
-                                              longitude: rawPad.longitude)
+                                  location: rawPad.location?.name ?? "-",
+                                  latitude: rawPad.latitude,
+                                  longitude: rawPad.longitude)
         }
         
         return Information.ViewModel(title: "Information",
-                                                 sections: sections,
-                                                 launch: launch,
-                                                 provider: provider,
-                                                 mission: mission,
-                                                 pad: pad)
+                                     sections: sections,
+                                     launch: launch,
+                                     provider: provider,
+                                     mission: mission,
+                                     pad: pad)
     }
     
 }
