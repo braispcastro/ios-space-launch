7dd37c58cf4b7bcf66d2163643f66838a1771cec
Brais P. Castro <braispcastro@gmail.com>
2022-01-22 18:37:09 +0100

    Added transitions

--- RocketLaunch/Modules/Components/NavigationController.swift
+++ RocketLaunch/Modules/Components/NavigationController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class NavigationController: UINavigationController {
-
+    
     // MARK: - Init
     
     public override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
@@ -37,11 +37,27 @@ class NavigationController: UINavigationController {
 
 extension UINavigationController {
     
+    public func pushViewController(_ viewController: UIViewController, completion: (() -> Void)? = nil) {
+        let transition: CATransition = CATransition()
+        transition.duration = 0.3
+        transition.type = CATransitionType.fade
+        view.layer.add(transition, forKey: nil)
+        pushViewController(viewController, animated: false, completion: completion)
+    }
+    
     public func pushViewController(_ viewController: UIViewController, animated: Bool, completion: (() -> Void)? = nil) {
         CATransaction.begin()
         CATransaction.setCompletionBlock(completion)
         pushViewController(viewController, animated: animated)
         CATransaction.commit()
+    }
+    
+    public func popViewController(completion: (() -> Void)? = nil) {
+        let transition: CATransition = CATransition()
+        transition.duration = 0.3
+        transition.type = CATransitionType.fade
+        view.layer.add(transition, forKey: nil)
+        popViewController(animated: false, completion: completion)
     }
     
     public func popViewController(animated: Bool, completion: (() -> Void)? = nil) {
@@ -50,5 +66,4 @@ extension UINavigationController {
         popViewController(animated: animated)
         CATransaction.commit()
     }
-    
 }
